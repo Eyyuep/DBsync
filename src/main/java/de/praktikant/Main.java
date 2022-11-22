2161ae93758db99e96fd8f083ea1d17e1e5b7c76
Leons <68811303+Gromidev@users.noreply.github.com>
2022-11-22 12:13:10 +0100

    Develop (#7)
    * Change folder structure
    
    * Working state
    
    * PostgreSQL in pom eingefügt
    
    * improve connection string
    
    * connected Databases
    
    * - Added equals method to username comparison
    - Added a warning to tablename conflict
    - Cleanup .gitignore
    
    * Bugfix and structure
    
    * creation of a property file and outsourcing of login instances
    
    * properties update and delete unnessary code and comments
    
    * delete create_properties.java
    
    * rename properties filename and add new keys
    
    * connection variables exported.
    properties data created.
    
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>
    
    Co-authored-by: FlorianMihm <flormihm@gmail.com>
    Co-authored-by: anasbe3055 <102251507+anasbe3055@users.noreply.github.com>
    Co-authored-by: nartesh <nartesh@yahoo.com>
    Co-authored-by: Eyyuep <115067739+Eyyuep@users.noreply.github.com>
    Co-authored-by: Eyyuep <E.baran1@hotmail.de>
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>

--- src/main/java/de/praktikant/Main.java
+++ src/main/java/de/praktikant/Main.java
@@ -1,97 +1,97 @@
 package de.praktikant;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.*;  
+import java.io.*;
+
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-
-        //Class.forName("org.h2.Driver");
-
-        String connectionKonnektor = "jdbc:h2:./test/db1";
-        String connectionPortal = "jdbc:h2:./test/db2";
-
-        Connection con = getConnection(connectionKonnektor);
-        Connection connection = getConnection(connectionPortal);
-
-        createTable(con);
-        createTable(connection);
-
-        createDataSet(con);
-        createDataSet(connection);
-
-        showDB(con);
-        showDB(connection);
-
-        /*editData(con);
-        editData(connection);*/
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, Exception {
 
 
+        FileReader reader = new FileReader("src\\main\\java\\de\\praktikant\\db.config.properties");  
+        Properties p = new Properties();  
+        p.load(reader);  
+
+        /* Testdaten zum aufrufen von db.properties
+        System.out.println(p.getProperty("username"));  
+        System.out.println(p.getProperty("password")); 
+        System.out.println(p.getProperty("database1"));  
+        System.out.println(p.getProperty("database2")); 
+        System.out.println(p.getProperty("db1_tbl")); 
+        */
+
+        String table1 = p.getProperty("db1_tbl");
+        String table2 = p.getProperty("db2_tbl");
+        String database1 = p.getProperty("database1");
+        String database2 = p.getProperty("database2");
+
+
+        final String connectionKonnektor = "jdbc:postgresql://localhost/" + database1 + "?user="+ p.getProperty("username") + "&password=" + p.getProperty("password");
+        final String connectionPortal = "jdbc:postgresql://localhost/" + database2 + "?user="+ p.getProperty("username") + "&password=" + p.getProperty("password");
+
+        System.out.print("Verbindung zu DB1: ");
+        Connection conKonnektor = getConnection(connectionKonnektor);
+        System.out.print("Verbindung zu DB2: ");
+        Connection conPortal = getConnection(connectionPortal);
+
+        System.out.println("");
+        System.out.println("Daten aus der deine DB: " + database1);
+        showDB(conKonnektor, table1);
+        System.out.println("Daten aus der deine DB: " + database2);
+        showDB(conPortal, table2);
+        
+        
+        System.out.println("Vergleich Ergebnisse:");
+        compareData(conKonnektor, conPortal, table1, table2);
+        System.out.println("");
     }
-
+    
     public static Connection getConnection(String connectionString) {
 
         Connection con = null;
-
+    
         try {
             con = DriverManager.getConnection(connectionString);
-            System.out.println("Hat geklappt");
+            System.out.println("Hat geklappt!");
         } catch (SQLException e) {
+            e.getMessage();
             e.printStackTrace();
         }
         return con;
     }
 
-    public static void createTable(Connection con) throws SQLException {
-        // Identity wird zu Serial
-        String query = "create table if not exists tabelle_1" +
-                "(id IDENTITY," +
-                "username varchar(50));";
-
-        con.createStatement().execute(query);
-
-    }
-
-    public static void createDataSet(Connection con) throws SQLException {
-
-        String query = "insert into tabelle_1(username)" +
-                "values('Manfred');";
-
-        con.createStatement().execute(query);
-
-    }
-
-    public static void showDB(Connection con) throws SQLException {
-
-        String query = "select * from tabelle_1;";
+    public static void showDB(Connection con, String table) throws SQLException {
+    
+        String formatedStringSQL = String.format("select * from %S", table);
+        String query = formatedStringSQL;
 
         ResultSet ergebnis = con.createStatement().executeQuery(query);
 
         while (ergebnis.next()) {
             System.out.println(ergebnis.getString("id") + " - " + ergebnis.getString("username"));
         }
-
+        System.out.println();
     }
 
-    public static void editData(Connection con) throws SQLException {
+    public static void compareData(Connection db1, Connection db2, String tbl1, String tbl2) throws SQLException {
 
-        String query = "update tabelle_1 set username = 'ronaldinho' where id = 2";
+        String query1 = "select * from " + tbl1;
+        String query2 = "select * from " + tbl2; 
+ 
 
-        con.createStatement().executeUpdate(query);
+        ResultSet ergebnis1 = db1.createStatement().executeQuery(query1);
+        ResultSet ergebnis2 = db2.createStatement().executeQuery(query2);
 
+        while(ergebnis1.next()) {
+            ergebnis2.next();
+            if(!(ergebnis1.getString("id").equals(ergebnis2.getString("id")))) {
+                System.out.println("ID " + ergebnis1.getString("id") + " ist nicht identisch mit " + ergebnis2.getString("id"));
+            }
+            if(!(ergebnis1.getString("username").equals(ergebnis2.getString("username")))) {
+                System.out.println(ergebnis1.getString("username") + " ist nicht identisch mit "  + ergebnis2.getString("username"));
+            } 
+        }
     }
-
-    public static void compareData(Connection db1, Connection db2) throws SQLException {
-
-        String query = "select db1.tabelle_1.*, db2.tabelle_1.* from db1.tabelle_1, db2.tabelle_1";
-
-        db1.createStatement().executeUpdate(query);
-        db2.createStatement().executeUpdate(query);
-
-    }
-
-
 }
