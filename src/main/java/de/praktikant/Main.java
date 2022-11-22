ee0f14f5acb545c3dd8f31bd1d768a18c86763b8
nartesh <nartesh@yahoo.com>
2022-11-22 14:47:35 +0100

    string formatting solved
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>

--- src/main/java/de/praktikant/Main.java
+++ src/main/java/de/praktikant/Main.java
@@ -14,21 +14,14 @@ public class Main {
         Properties p = new Properties();  
         p.load(reader);  
 
-        /* Testdaten zum aufrufen von db.properties
-        System.out.println(p.getProperty("username"));  
-        System.out.println(p.getProperty("password")); 
-        System.out.println(p.getProperty("database1"));  
-        System.out.println(p.getProperty("database2")); 
-        System.out.println(p.getProperty("db1_tbl")); 
-        */
-
         String table1 = p.getProperty("db1_tbl");
         String table2 = p.getProperty("db2_tbl");
         String database1 = p.getProperty("database1");
         String database2 = p.getProperty("database2");
 
-        //String connectionDB1 = String.format("jdbc:postgresql://localhost/ %S", database1, "?user= %S",  p.getProperty("username"), "&password= %S", p.getProperty("password"));
-        
+        //String connectionDB1 = String.format("jdbc:postgresql://localhost/ %1$s ?user= %2$s &password= %3$s", database1, p.getProperty("username"), p.getProperty("password"));
+        //String connectionDB2 = String.format("jdbc:postgresql://localhost/ %1$s ?user= %2$s &password= %3$s", database2, p.getProperty("username"), p.getProperty("password"));
+
         final String connectionKonnektor = "jdbc:postgresql://localhost/" + database1 + "?user="+ p.getProperty("username") + "&password=" + p.getProperty("password");
         final String connectionPortal = "jdbc:postgresql://localhost/" + database2 + "?user="+ p.getProperty("username") + "&password=" + p.getProperty("password");
 
@@ -45,10 +38,11 @@ public class Main {
         System.out.println(printDB2);
         showDB(conPortal, table2);
         
-        
+
         System.out.println("Vergleich Ergebnisse:");
         compareData(conKonnektor, conPortal, table1, table2);
         System.out.println("");
+
     }
     
     public static Connection getConnection(String connectionString) {
@@ -67,42 +61,43 @@ public class Main {
 
     public static void showDB(Connection con, String table) throws SQLException {
     
-        String formatedStringSQL = String.format("select * from %S", table);
+        String formatedStringSQL = String.format("select * from %s", table);
         String query = formatedStringSQL;
 
         ResultSet ergebnis = con.createStatement().executeQuery(query);
 
         while (ergebnis.next()) {
-            String ergebnisIDundUsername = String.format("REsult: %1$d and %2$s", ergebnis.getString("id"), ergebnis.getString("username"));
+            String ergebnisIDundUsername = String.format("ID is: %1$s und Username is: %2$s", ergebnis.getString("id"), ergebnis.getString("username"));;
             System.out.println(ergebnisIDundUsername);
             //System.out.println(ergebnis.getString("id") + " - " + ergebnis.getString("username"));
         }
         System.out.println();
+
     }
 
     public static void compareData(Connection db1, Connection db2, String tbl1, String tbl2) throws SQLException {
         
-        String abfrageTB1 = String.format("select * from %S ", tbl1);
+        String abfrageTB1 = String.format("select * from %s ", tbl1);
         String query1 = abfrageTB1;
-        String abfrageTB2 = String.format("select * from %S ", tbl2);
+        String abfrageTB2 = String.format("select * from %s ", tbl2);
         String query2 = abfrageTB2; 
  
-
         ResultSet ergebnis1 = db1.createStatement().executeQuery(query1);
         ResultSet ergebnis2 = db2.createStatement().executeQuery(query2);
 
         while(ergebnis1.next()) {
             ergebnis2.next();
             if(!(ergebnis1.getString("id").equals(ergebnis2.getString("id")))) {
-                String printErgebnisID = String.format("1%$d and 2%$d", ergebnis1.getString("id"), ergebnis2.getString("id"));
+                String printErgebnisID = String.format("ID %1$s ist nicht identisch mit %2$s", ergebnis1.getString("id"), ergebnis2.getString("id"));
                 System.out.println(printErgebnisID);
                 //System.out.println("ID " + ergebnis1.getString("id") + " ist nicht identisch mit " + ergebnis2.getString("id"));
             }
             if(!(ergebnis1.getString("username").equals(ergebnis2.getString("username")))) {
-                String printErgebnisUsername = String.format(ergebnis1.getString("username"), " ist nicht identisch mit %S " , ergebnis2.getString("username"));
+                String printErgebnisUsername = String.format("Username %1$s ist nicht identisch mit %2$s", ergebnis1.getString("username"), ergebnis2.getString("username"));
                 System.out.println(printErgebnisUsername);
                 //System.out.println(ergebnis1.getString("username") + " ist nicht identisch mit "  + ergebnis2.getString("username"));
             }
         }
     }
+    
 }
