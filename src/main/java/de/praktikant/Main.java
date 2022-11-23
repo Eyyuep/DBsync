52525f0b3a461386a8f82596fc35bd9676ca1d7f
nartesh <nartesh@yahoo.com>
2022-11-23 10:51:30 +0100

    sys.out exchange with logger slf4j method
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>

--- src/main/java/de/praktikant/Main.java
+++ src/main/java/de/praktikant/Main.java
@@ -11,13 +11,13 @@ public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException, Exception {
         
         Logger logger = LoggerFactory.getLogger(Main.class);
-        logger.info("Hello World");
-        
+        //^logger.warn("Ich bin logger: Warnung");
         
 
         FileReader reader = new FileReader("src\\main\\java\\de\\praktikant\\db.config.properties");  
         Properties p = new Properties();  
         p.load(reader);  
+
 
         String table1 = p.getProperty("db1_tbl");
         String table2 = p.getProperty("db2_tbl");
@@ -30,33 +30,34 @@ public class Main {
         final String connectionKonnektor = "jdbc:postgresql://localhost/" + database1 + "?user="+ p.getProperty("username") + "&password=" + p.getProperty("password");
         final String connectionPortal = "jdbc:postgresql://localhost/" + database2 + "?user="+ p.getProperty("username") + "&password=" + p.getProperty("password");
 
-        System.out.print("Verbindung zu DB1: ");
+        logger.info("Verbindung zu DB1: ");
         Connection conKonnektor = getConnection(connectionKonnektor);
-        System.out.print("Verbindung zu DB2: ");
+        logger.info("Verbindung zu DB2: ");
         Connection conPortal = getConnection(connectionPortal);
 
-        System.out.println("");
+        logger.info("");
         String printDB1 = String.format("Daten aus der deine DB: %s ", database1);
-        System.out.println(printDB1);
+        logger.info(printDB1);
         showDB(conKonnektor, table1);
         String printDB2 = String.format("Daten aus der deine DB: %s ", database2);
-        System.out.println(printDB2);
+        logger.info(printDB2);
         showDB(conPortal, table2);
         
 
-        System.out.println("Vergleich Ergebnisse:");
+        logger.info("Vergleich Ergebnisse:");
         compareData(conKonnektor, conPortal, table1, table2);
-        System.out.println("");
+        logger.info("");
 
     }
     
     public static Connection getConnection(String connectionString) {
 
         Connection con = null;
+        Logger logger = LoggerFactory.getLogger(Main.class);
     
         try {
             con = DriverManager.getConnection(connectionString);
-            System.out.println("Hat geklappt!");
+            logger.info("Hat geklappt!");
         } catch (SQLException e) {
             e.getMessage();
             e.printStackTrace();
@@ -66,6 +67,8 @@ public class Main {
 
     public static void showDB(Connection con, String table) throws SQLException {
     
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
         String formatedStringSQL = String.format("select * from %s", table);
         String query = formatedStringSQL;
 
@@ -73,15 +76,17 @@ public class Main {
 
         while (ergebnis.next()) {
             String ergebnisIDundUsername = String.format("ID is: %1$s und Username is: %2$s", ergebnis.getString("id"), ergebnis.getString("username"));;
-            System.out.println(ergebnisIDundUsername);
-            //System.out.println(ergebnis.getString("id") + " - " + ergebnis.getString("username"));
+            logger.info(ergebnisIDundUsername);
+            //logger.info(ergebnis.getString("id") + " - " + ergebnis.getString("username"));
         }
-        System.out.println();
+        logger.info("");
 
     }
 
     public static void compareData(Connection db1, Connection db2, String tbl1, String tbl2) throws SQLException {
         
+        Logger logger = LoggerFactory.getLogger(Main.class);
+
         String abfrageTB1 = String.format("select * from %s ", tbl1);
         String query1 = abfrageTB1;
         String abfrageTB2 = String.format("select * from %s ", tbl2);
@@ -94,13 +99,13 @@ public class Main {
             ergebnis2.next();
             if(!(ergebnis1.getString("id").equals(ergebnis2.getString("id")))) {
                 String printErgebnisID = String.format("ID %1$s ist nicht identisch mit %2$s", ergebnis1.getString("id"), ergebnis2.getString("id"));
-                System.out.println(printErgebnisID);
-                //System.out.println("ID " + ergebnis1.getString("id") + " ist nicht identisch mit " + ergebnis2.getString("id"));
+                logger.warn(printErgebnisID);
+                //logger.info("ID " + ergebnis1.getString("id") + " ist nicht identisch mit " + ergebnis2.getString("id"));
             }
             if(!(ergebnis1.getString("username").equals(ergebnis2.getString("username")))) {
                 String printErgebnisUsername = String.format("Username %1$s ist nicht identisch mit %2$s", ergebnis1.getString("username"), ergebnis2.getString("username"));
-                System.out.println(printErgebnisUsername);
-                //System.out.println(ergebnis1.getString("username") + " ist nicht identisch mit "  + ergebnis2.getString("username"));
+                logger.warn(printErgebnisUsername);
+                //logger.info(ergebnis1.getString("username") + " ist nicht identisch mit "  + ergebnis2.getString("username"));
             }
         }
     }
