09124c8dd0d4b955720ec988c3f8277cfd40280d
Eyyuep <115067739+Eyyuep@users.noreply.github.com>
2022-11-24 14:34:03 +0100

    Develop (#8)
    * Change folder structure
    
    * Working state
    
    * PostgreSQL in pom eingefügt
    
    * improve connection string
    
    * connected Databases
    
    * - Added equals method to username comparison
    - Added a warning to tablename conflict
    - Cleanup .gitignore
    
    * Bugfix and structure
    
    * creation of a property file and outsourcing of login instances
    
    * properties update and delete unnessary code and comments
    
    * delete create_properties.java
    
    * rename properties filename and add new keys
    
    * connection variables exported.
    properties data created.
    
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>
    
    * string formating unfinished
    
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>
    
    * Update db.config.properties
    
    * string formatting solved
    
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>
    
    * logger slf4j connection does not work
    
    * logger slf4j connection does not work
    
    * sys.out exchange with logger slf4j method
    
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    
    * Update db.config.properties
    
    * methods outsource
    
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    
    * SQLException try-catch block  implemented
    
    Co-authored-by: FlorianMihm <flormihm@gmail.com>
    Co-authored-by: anasbe3055 <102251507+anasbe3055@users.noreply.github.com>
    Co-authored-by: nartesh <nartesh@yahoo.com>
    Co-authored-by: Leons <Gromidev@users.noreply.github.com>
    Co-authored-by: Eyyuep <Eyyuep@users.noreply.github.com>
    Co-authored-by: Anas3055 <Anas3055@users.noreply.github.com>
    Co-authored-by: nartesh <74494162+nartesh@users.noreply.github.com>

---
+++ src/main/java/de/praktikant/showDB.java
@@ -0,0 +1,30 @@
+package de.praktikant;
+
+import java.sql.*;
+import org.slf4j.*;
+
+public class showDB {
+    
+    public void myShowDB(Connection con, String table) throws SQLException {
+        
+        try {
+            Logger logger = LoggerFactory.getLogger(Main.class);
+
+            String formatedStringSQL = String.format("select * from %s", table);
+            String query = formatedStringSQL;
+
+            ResultSet ergebnis = con.createStatement().executeQuery(query);
+            
+            while (ergebnis.next()) {
+                String ergebnisIDundUsername = String.format("ID %1$s hat Username %2$s", ergebnis.getString("id"), ergebnis.getString("username"));;
+                logger.info(ergebnisIDundUsername);
+                //logger.info(ergebnis.getString("id") + " - " + ergebnis.getString("username"));
+            }
+            logger.info("");
+      
+        } catch (Exception e) {
+            e.getMessage();
+        }
+        
+    }
+}
