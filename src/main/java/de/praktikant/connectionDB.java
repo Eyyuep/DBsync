b6c8a747626b42a6033e48cd745d4c995fda3976
nartesh <74494162+nartesh@users.noreply.github.com>
2022-11-25 12:01:32 +0100

    Update connectionDB.java

--- src/main/java/de/praktikant/connectionDB.java
+++ src/main/java/de/praktikant/connectionDB.java
@@ -4,18 +4,22 @@ import java.sql.*;
 import org.slf4j.*;
 
 public class connectionDB {
+
     public Connection getConnection(String connectionString) {
 
         Connection con = null;
+
         Logger logger = LoggerFactory.getLogger(Main.class);
     
         try {
             con = DriverManager.getConnection(connectionString);
             logger.info("Hat geklappt!");
+            
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return con;
+        
     }
     
 }
